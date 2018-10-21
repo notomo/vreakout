514c86bf58d55605b7547d572d2ca332e6e18aca
notomo <notomo.motono@gmail.com>
2018-10-21 16:21:25 +0900

    Fix the ball collision

--- src/objects/ball.ts
+++ src/objects/ball.ts
@@ -10,7 +10,7 @@ export class Ball extends Actor {
       width: Ball.RADIUS * 2,
       height: Ball.RADIUS * 2,
       color: Color.Chartreuse,
-      collisionType: CollisionType.Passive
+      collisionType: CollisionType.Passive,
     });
 
     this.vel.setTo(velX, velY);
@@ -21,10 +21,44 @@ export class Ball extends Actor {
       }
 
       const intersection = ev.intersection.normalize();
-      if (Math.abs(intersection.x) > Math.abs(intersection.y)) {
-        this.vel.x *= -1;
-      } else {
-        this.vel.y *= -1;
+      const other = ev.other;
+
+      const inRangeY = other.getTop() <= this.y && this.y <= other.getBottom();
+      if (Math.abs(intersection.x) > 0 && inRangeY) {
+        if (
+          other.x < this.x &&
+          this.getLeft() <= other.getRight() &&
+          this.vel.x < 0
+        ) {
+          // ball left collision
+          this.vel.x *= -1;
+        } else if (
+          other.x > this.x &&
+          this.getRight() >= other.getLeft() &&
+          this.vel.x > 0
+        ) {
+          // ball right collision
+          this.vel.x *= -1;
+        }
+      }
+
+      const inRangeX = other.getLeft() <= this.x && this.x <= other.getRight();
+      if (Math.abs(intersection.y) > 0 && inRangeX) {
+        if (
+          other.y < this.y &&
+          this.getTop() <= other.getBottom() &&
+          this.vel.y < 0
+        ) {
+          // ball top collision
+          this.vel.y *= -1;
+        } else if (
+          other.y > this.y &&
+          this.getBottom() >= other.getTop() &&
+          this.vel.y > 0
+        ) {
+          // ball bottom collision
+          this.vel.y *= -1;
+        }
       }
     });
   }
